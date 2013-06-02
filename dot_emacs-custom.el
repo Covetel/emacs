953b5032eb2d002dc8d3a754650899f033f6777d
Walter Vargas <walter@covetel.com.ve>
2013-06-02 12:27:25 -0430

    adding a place to personal configuration

--- dot_emacs-custom.el
+++ dot_emacs-custom.el
@@ -1,15 +1,7 @@
-;; Remember:
-;; use (setq ...) to set value locally to a buffer
-;; use (setq-default ...) to set value globally
-
 ;; theme
 (load-theme 'dichromacy t)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes (quote (dichromacy)))
  '(inhibit-startup-screen t)
@@ -20,19 +12,9 @@
  '(tool-bar-mode nil)
  '(woman-locale "es_ES.UTF-8"))
 
-;;(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-; '(default ((t (:stipple nil :background "darkslategrey" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 122 :width normal :foundry "xos4" :family "Terminus")))))
-
 ;; Turn on auto fill mode automatically in al modes 
 (auto-fill-mode 1)
 (setq-default fill-column 78)
-
-;; highlight current line minor mode
-;;(global-hl-line-mode 1)
 
 ;; emacs backups
 (setq make-backup-files t)
@@ -44,10 +26,11 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(cperl-array-face ((t (:weight normal))) t)
  '(cperl-hash-face ((t (:weight normal))) t))
+
+;; Personal configuration here - personalconf
+
+
